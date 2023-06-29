9e225429feddc3121db9839d08497ec1c5f2d616
PiotrDrechsler <pdrech@gmail.com>
2023-06-29 12:19:35 +0200

    Update users.js
    Sending verification url for user's email works now fine. You can click now into verification link, and your verify should be completed.

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,12 @@
-const { User, hashPassword } = require("../models/user");
-const gravatar = require("gravatar");
-const nodemailer = require("nodemailer");
-const { v4: uuidv4 } = require("uuid");
+const { User, hashPassword } = require('../models/user')
+const gravatar = require('gravatar')
+const nodemailer = require('nodemailer')
+const { v4: uuidv4 } = require('uuid')
 
 const createUser = async (password, email, subscription, avatarURL, token) => {
   try {
-    const hashedPassword = hashPassword(password);
-    const createAvatar = gravatar.url(email, { d: "robohash", s: "250" });
+    const hashedPassword = hashPassword(password)
+    const createAvatar = gravatar.url(email, { d: 'robohash', s: '250' })
 
     const user = new User({
       password: hashedPassword,
@@ -15,92 +15,93 @@ const createUser = async (password, email, subscription, avatarURL, token) => {
       avatarURL: createAvatar,
       verify: false,
       verificationToken: uuidv4(),
-      token,
-    });
+      token
+    })
 
-    await user.save();
+    await user.save()
 
-    await sendVerificationEmail(user.email, user.verificationToken);
+    await sendVerificationEmail(user.email, user.verificationToken)
 
-    return user;
+    return user
   } catch (err) {
-    console.log(err);
-    throw err;
+    console.log(err)
+    throw err
   }
-};
+}
 
 const getAllUsers = async () => {
-  const users = await User.find();
-  return users;
-};
+  const users = await User.find()
+  return users
+}
 
 const getUserById = async (_id) => {
-  const user = await User.findOne({ _id });
-  return user;
-};
+  const user = await User.findOne({ _id })
+  return user
+}
 
 const getUserByEmail = async (email) => {
-  const user = await User.findOne({ email });
-  return user;
-};
+  const user = await User.findOne({ email })
+  return user
+}
 
 const addUserToken = async (id, token) => {
-  return User.findByIdAndUpdate(id, { token });
-};
+  return User.findByIdAndUpdate(id, { token })
+}
 
 const updateUserToken = async (_id) => {
-  return User.findOneAndUpdate(_id, { token: null });
-};
+  return User.findOneAndUpdate(_id, { token: null })
+}
 
 const updateAvatar = async (email, avatarURL) => {
   const user = await User.findOneAndUpdate(
     { email },
     { avatarURL },
     { new: true }
-  );
-  return user;
-};
+  )
+  return user
+}
 
 const verifyUser = async (verificationToken) => {
   const user = await User.findOneAndUpdate(
     { verificationToken },
     { verify: true, verificationToken: null },
     { new: true }
-  );
-  return user;
-};
+  )
+  return user
+}
 
 const sendVerificationEmail = async (email, verificationToken) => {
   try {
-    const auth = await nodemailer.createTestAccount();
+    const auth = await nodemailer.createTestAccount()
 
     const transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
-      secure: false,
-      auth,
-    });
+      service: 'gmail',
+      auth: {
+        user: 'pdrech@gmail.com',
+        pass: 'nsskwzxcxdfzgkov'
+      }
+    })
 
     const html = `
       <div>
-        <p>Click <a href="http://localhost:3000/api/users/verify/${verificationToken}">here</a> to verify your account</p>
+        <p>Click <a href="http://localhost:3030/api/users/verify/${verificationToken}">here</a> to verify your account</p>
       </div>
-    `;
+    `
 
     const info = await transporter.sendMail({
-      from: { name: "Piotr", address: "example@example.com" },
+      from: { name: 'Piotr', address: 'example@example.com' },
       to: email,
-      subject: "Verify your account",
-      html,
-    });
+      subject: 'Verify your account',
+      html
+    })
 
-    const previewUrl = nodemailer.getTestMessageUrl(info);
-    console.log(previewUrl);
+    const previewUrl = nodemailer.getTestMessageUrl(info)
+    console.log(previewUrl)
   } catch (err) {
-    console.log(err);
-    throw err;
+    console.log(err)
+    throw err
   }
-};
+}
 
 module.exports = {
   createUser,
@@ -111,5 +112,5 @@ module.exports = {
   getUserByEmail,
   updateAvatar,
   verifyUser,
-  sendVerificationEmail,
-};
+  sendVerificationEmail
+}
